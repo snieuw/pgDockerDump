841086e0a7d197d32d785036f0635a073f32d66e
Steffen Nieuwenhoven <steffen.nieuwenhoven@masterkey.nl>
2021-08-05 14:34:42 +0200

    update

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # pgDockerDump
 Create PostegreSQL dumps by running a single docker container
+
+This container is targeting Docker or Kubernetes environments that are used to create database backups. The database does not have to be containerized. 
+
+## Environmen variables
+The container supports the following environmental variables:
+
+|Name|Description|Mandatory|Default value|
+--------------|-----------------|---------|--------------
+|PGDUMP_DATABASE    | Name of the database to backup | yes   | postgres |
+|PGDUMP_HOST        | Hostname | no    | localhost (127.0.0.1) |
+|PGDUMP_PORT        | Port of the postgres server | no    | 5432 |
+|PGDUMP_USER        | Username used for connection | no    | postgres |
+|PGDUMP_PWD         | Password used for connection | no | - |
+|PGDUMP_DELETE_OLD  | Should old backups be deleted? | no | false | 
+|PGDUMP_RETENTION   | How many days should the backups be stored? | no | 30 |
+
+
+## Volumes
+The following folder can be mounted on a volume to store the backup data persistently
+
+`/backup`
+
+
+## Examples
+Example kubernetes integration:
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,22 +1,33 @@
 #! /bin/bash
 
+# Set default values
+DB=${PGDUMP_DATABASE:-"postgres"}
+HOST=${PGDUMP_HOST:-"127.0.0.1"}
+PORT=${PGDUMP_PORT:-5432}
+USER=${PGDUMP_USER:-"postgres"}
+DEL_OLD=${PGDUMP_DELETE_OLD:-false}
+HOST=${PGDUMP_RETENTION:-14}
+
+# set the PGPASSWORD environment variable, for connecting to the server via username/password
+export PGPASSWORD=$PGDUMP_PWD
+
 #format the current date and time
 NOW=$(date +"%Y%m%d-%H%M%S")
 
 #use the formated date and time to create a target filename for the dump
-ARCHIVE="/backup/${PGDUMP_DATABASE}-$NOW.archive"
+ARCHIVE="/backup/${DB}-$NOW.archive"
 
 #test if the backup folder already exists. Create one if it does not exist yet. Prevents errors in the log
 ! test -d /backup && mkdir /backup/
 
 #logging info
-echo "Dumping $PGDUMP_DATABASE to $ARCHIVE"
+echo "Dumping $DB to $ARCHIVE"
 
 #this creates the actual dump
-pg_dump -h $PGDUMP_HOST -p $PGDUMP_PORT -U $PGDUMP_USER -Fc -f $ARCHIVE $PGDUMP_DATABASE
+pg_dump -h $HOST -p $PORT -U $USER -Fc -f $ARCHIVE $DB
 
 #finished
 echo "Backup complete"
 
 #clean up old backup files
-find . -name $PGDUMP_DATABASE"-*.archive" -type f -mtime +$PGDUMP_RETENTION -exec rm -f {} \;
+find . -name $DB"-*.archive" -type f -mtime +$PGDUMP_RETENTION -exec rm -f {} \;
